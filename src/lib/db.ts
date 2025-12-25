e76061641af0e42ace6320c69840851557b5c9de
Kbro1989 <149967879+Kbro1989@users.noreply.github.com>
2025-12-24 23:11:52 -0600

    fix: update InstantDB App ID to match user credentials

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,7 @@ import schema from "../instant.schema";
 
 // Pick of Gods Configuration
 // Using the Client ID provided by the project settings
-const INSTANT_APP_ID = "13f19b79-d6e2-49b2-b26a-630243051890";
+const INSTANT_APP_ID = "af4e550c-12a0-400c-913e-610161182ee7";
 
 export const db = init({
     appId: INSTANT_APP_ID,
